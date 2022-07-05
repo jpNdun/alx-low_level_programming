54f9e109e43e67c332cba133a2651e575126f6ba
jpNdun <ndungutsej0@gmail.com>
2022-07-05 08:01:52 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("last digit of %d is %d and is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, lastn)
+printf("last digit of %d is %d and is 0\n", n, lastn);
 }
 else
 {
