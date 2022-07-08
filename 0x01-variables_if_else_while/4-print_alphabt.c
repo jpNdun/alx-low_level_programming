24ed96b820f72094ded645ccbb1613265b14ae36
jpNdun <ndungutsej0@gmail.com>
2022-07-08 08:15:19 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-if (alphabet == 'q' && alphabet == 'e')
+if (alphabet == 'q' || alphabet == 'e')
 {
+continue;
+}
 putchar (alphabet);
-}
-}
 putchar ('\n');
 return (0);
 }
