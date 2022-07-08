e041aab415aa83ffcde53ffbb2c199dcf19624ac
jpNdun <ndungutsej0@gmail.com>
2022-07-08 16:16:29 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,36 @@
-#include "main.h"
+#include"main.h"
 /**
- * times_table - prints the times table from 0 - 9
- *
- * Return: Nothing
- */
-void times_table(void)
+* times_table - prints the times table from 0 - 9
+*
+* Return: Nothing
+*/
+voidtimes_table(void)
 {
-	int i, j, res;
+int i, j, res;
 
-	for (i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
 	{
-		for (j = 0; j <= 9; j++)
+		res = (i * j);
+		if (j != 0)
 		{
-			res = (i * j);
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (res >= 10)
-			{
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
-			else if (res < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar((res % 10) + '0');
-			}
-			else
-				_putchar((res % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
-		_putchar('\n');
+		if (res >= 10)
+		{
+			_putchar((res / 10) + '0');
+			_putchar((res % 10) + '0');
+		}
+		else if (res < 10 && j != 0)
+		{
+			_putchar(' ');
+			_putchar((res % 10) + '0'):
+		}
+		else
+			_putchar((res % 10) + '0');
 	}
-}	
+	_putchar('\n');
+}
+}
