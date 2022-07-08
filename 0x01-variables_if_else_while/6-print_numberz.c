bbecb9c23eea2bcf1b138b109cf00e0623d23168
jpNdun <ndungutsej0@gmail.com>
2022-07-08 04:45:18 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,9 +4,14 @@
  *
  * Return: always returns 0
  */
-int a;
-for (; a < 10; a++)
-putchar('0' + a);
+int main(void);
+{
+int i = '0';
+while (i <= '9')
+{
+putchar(i);
+i++;
+}
 putchar('\n');
 return (0);
 }
