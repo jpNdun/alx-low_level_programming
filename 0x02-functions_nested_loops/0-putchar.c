83711afa6f86b20f40b828590fc7bb82106f5ca8
jpNdun <ndungutsej0@gmail.com>
2022-07-08 13:08:22 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
-* main - prints 'demo'
-*
-* Return: nothing
-*/
+ * main - prints putchar
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-_putchar('d');
-_putchar('e');
-_putchar('m');
-_putchar('o');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0); 
