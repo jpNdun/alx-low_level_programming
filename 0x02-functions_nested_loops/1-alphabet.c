04a81b79f918c80663710045fb7d09b63ddbeee3
jpNdun <ndungutsej0@gmail.com>
2022-07-08 14:00:03 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,12 @@
  * print_alphabet -> prints the lowercase alphabets
  * Return: nothing
  */
-void main(void)
+void print_alphabet(void)
 {
 int j;
-print_alphabet();
 for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
 _putchar('\n');
-return (0);
 }
