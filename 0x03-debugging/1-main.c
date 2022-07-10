7c3d60c924dc023fd6ef769e54deec965847ca80
jpNdun <ndungutsej0@gmail.com>
2022-07-10 01:50:28 -0700

    hot fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,18 +4,19 @@
  * Return: 0
  */
 int main(void)
-{
+{	
 	int i;
 
 	printf("Infiinite loop incoming :(\n");
-
+	
 	i = 0;
-	/**
-	 * while (i < 0)
-	 * {
-	 * 	putchar(i);
-	 * }
-	 */
+
+	
+	/*while (i < 0)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+	 
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
