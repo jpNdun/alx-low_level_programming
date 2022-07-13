ba7d7f0c0a9cac375777b386264ca363e251a272
jpNdun <ndungutsej0@gmail.com>
2022-07-13 08:39:18 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /** print_rev - a function that prints a string in reverse
- * fcounter is to first count to end, n is to count back
+ * fcounter: is to first count to end, n is to count back
  * @s: str input
  * Return: string in reverse
  */
