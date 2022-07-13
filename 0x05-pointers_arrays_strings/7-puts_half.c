058f0056c5f3b2aed1665d0d7713d64d5808314c
jpNdun <ndungutsej0@gmail.com>
2022-07-13 09:09:38 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,10 +15,10 @@ void puts_half(char *str)
 
 	n = (len / 2);
 
-	if ((len % 2) == )
+	if ((len % 2) == 1)
 		n = ((len + 1) / 2);
 
 	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	_putchar('\n')
+	_putchar('\n');
 }
