0a71c15bd2e8f4402ac62bdc0b91c77040c63316
jpNdun <ndungutsej0@gmail.com>
2022-07-16 12:56:05 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * cap_string - prints the uppercase of a string
@@ -9,19 +8,17 @@
  */
 char *cap_string(char *s)
 {
-	
 	int a = 0, i;
-	int cspc = 13;
+	int len = 13;
 	char spc[] = {32, 9, '\n', ',', ';','.', '!', '?', '"', '(', ')', '{', '}'};
 
 	while (s[a])
 	{
 		i = 0;
-
-		while (i < cspc)
+		while (i < len)
 		{
 			if ((a == 0 || s[a - 1] == spc[i] && (s[a] >= 97 && s[a] <= 122))
-					s[a] -= 32;
+				s[a] -= 32;
 			i++;
 		}
 
