afb390f68912e887753f149cf32df4b21ee40f5a
jpNdun <ndungutsej0@gmail.com>
2022-08-05 07:03:32 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 
 /**
  * print_name - prints a name
@@ -9,6 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
