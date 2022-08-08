d1b59f44564f6531213bfed8ff54186347731bbd
jpNdun <ndungutsej0@gmail.com>
2022-08-08 10:26:09 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list strings;
 
 	if (separator == NULL)
-		SEPARATOR = "";
+		separator = "";
 	va_start(strings, n);
 
 	for (i = 0; i < n; i++)
