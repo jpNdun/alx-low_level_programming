58a04bbd9e366ef8c84dfde2fc4eef4031924af4
jpNdun <ndungutsej0@gmail.com>
2022-08-25 13:50:02 -0700

    hotfix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	file = open(finame, O_RDONLY);
+	file = open(filename, O_RDONLY);
 
 	if (file == -1)
 	{
